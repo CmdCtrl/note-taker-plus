3e6c9e1188134f3f136619c0f58bd084da4a4078
CmdCtrl <thecmdctrl@gmail.com>
2021-06-09 19:42:41 -0400

    note editing complete

--- src/app.js
+++ src/app.js
@@ -36,27 +36,62 @@ function addNote(note) {
     noteContainer.appendChild(noteElement);
 }
 
-//access DOM elements
+function publishEdit(note) {
+    let editedText = note.querySelector('.note-text').value;
+
+    note.addEventListener('click', (e) => {
+        try{
+            if(e.target.classList.contains('edit-btn')) {
+                editedText = note.querySelector('.note-text').value;
+                note.innerHTML = `<div class="note-header">
+                <span class="note-delete"><i class="fas fa-trash-alt"></i></span>
+                <span class="note-edit"><i class="fas fa-cog"></i></span>
+            </div>
+            <div class="note-body">
+                <p class="body-text">${editedText}</p>
+            </div>`
+
+            note.classList.remove('edit-btn');
+            isEditing = false;
+            }
+        } catch(err) {
+            if(!err instanceof TypeError){
+                console.warn(err);
+            }
+        }
+    });
+}
+
+//global variables
 const del = "fa-trash-alt";
 const edit = "fa-cog";
 let currentSelected = null;
+let isEditing = false;
 
+//event handling
 noteContainer.addEventListener('click', (e) => {
-    console.log(e.target);
-    if(e.target.classList.contains(edit)) {
+    if(e.target.classList.contains(edit) && !isEditing) {
+        isEditing = true;
         const curr = e.target.closest('.note');
         const currText = curr.querySelector('.body-text').textContent;
-        console.log(currText);
-        //create a modal
-        //allow user to fill and submit modalText
-        //currText = modalText
+        if(isEditing) {
+            curr.innerHTML = `<div class="note-header">
+                                <span class="note-delete"><i class="fas fa-trash-alt"></i></span>
+                                <span class="note-edit"><i class="fas fa-cog"></i></span>
+                              </div>
+                              <div class="note-body">
+                                <textarea cols="16" class="note-text">${currText}</textarea>
+                                <button class="edit-btn">Publish Edit</button>
+                              </div>`;
+            publishEdit(curr);
+        }
     }
     if(e.target.classList.contains(del)) {
         const curr = e.target.closest('.note');
         curr.remove();
     }
     if(currentSelected === null){
-        if(e.target.classList.contains('note-body') || e.target.classList.contains('body-text') ) {
+        if(e.target.classList.contains('note-body') || e.target.classList.contains('body-text')) {
             e.target.closest('.note').classList.add('onclick');
             currentSelected = e.target.closest('.note');
         }
@@ -69,11 +104,12 @@ noteContainer.addEventListener('click', (e) => {
             //swap condition
             let first = currentSelected.innerText;
             let second = e.target.closest('.note');
-
+    
             //remove selections
             e.target.closest('.note').classList.remove('onclick');
             currentSelected.classList.remove('onclick');
 
+            //perform HTML swap
             currentSelected.innerHTML = `<div class="note-header">
             <span class="note-delete"><i class="fas fa-trash-alt"></i></span>
             <span class="note-edit"><i class="fas fa-cog"></i></span>
@@ -90,6 +126,7 @@ noteContainer.addEventListener('click', (e) => {
                 <p class="body-text">${first}</p>
             </div>`;
 
+            //update state
             currentSelected = null;
             second = null;
         }
